acdf5a444facce7490f8ee640b36464147b637b6
UPWAN <96981533+UPWAN2011@users.noreply.github.com>
2023-03-06 22:55:49 +0530

    Add files via upload

---
+++ AI.js
@@ -0,0 +1,43 @@
+
+  
+  
+  
+ 
+ 
+
+function logout() {
+    window.location = "index.html";
+}
+
+function like() {
+    window.location = "like.html";
+}
+
+function alanturing(){
+    
+      window.location = "alan.html";
+  }
+
+  function johnmccarthy() {
+    
+    window.location = "john.html";
+}
+
+function rossquillan() {
+    
+    window.location = "ross.html";
+}
+
+function ibm() {
+    
+    window.location = "ibm.html";
+}
+
+function marvinminsky() {
+    
+    window.location = "alan.html";
+}
+
+function edwardfiegenbaum() {
+        window.location = "index.html";
+    }
